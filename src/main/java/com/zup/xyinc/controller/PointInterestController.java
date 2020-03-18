221f74c2c9bfeae136dfa1fa793ee462a0ad9e6e
Nycholas Sousa <nycholas.sousa@conductor.com.br>
2020-03-18 19:58:59 -0300

    Adjusting url to singular

--- src/main/java/com/zup/xyinc/controller/PointInterestController.java
+++ src/main/java/com/zup/xyinc/controller/PointInterestController.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/points")
+@RequestMapping("/point")
 public class PointInterestController {
 
     @Autowired
