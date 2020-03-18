12ee69dd4d3ce46c9ca10e4607489ada8964dd1d
Nycholas Sousa <nycholas.sousa@conductor.com.br>
2020-03-18 17:52:30 -0300

    First Commit

---
+++ src/main/java/com/zup/xyinc/xyinc/XyIncApplication.java
@@ -0,0 +1,13 @@
+package com.zup.xyinc.xyinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyIncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyIncApplication.class, args);
+    }
+
+}
